2d135e5e5918377a043696e601068ef99b44fddc
Sune Hellfritzsch <sunehellfritzsch@gmail.com>
2015-04-13 14:20:00 +0200

    The following was added: 1) Database setup, 2) Input fields for sign up and login pages

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
  
 app = Flask(__name__)
+
+# Tells Flask to use the database 'development'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysqlpw@localhost/development'
+
+# We import db, the usable instance of the SQLAlchemy class we created in models.py, and binds it to our app
+# Now db knows that it has to use the development database, and we can query it through our db-object
+from models import db
+db.init_app(app)
  
 import webapp.routes
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,6 +1,8 @@
 from webapp import app
 from flask import Flask, render_template
 
+from models import db
+
 # Mapping the URL / to the function home(). When someone visits / then home() will execute
 # home() is using the render_template function to render the template home.html
 @app.route('/')
@@ -26,3 +28,11 @@ def signup():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+# Routing for testing if the database is working
+@app.route('/testdb')
+def testdb():
+  if db.session.query("1").from_statement("SELECT 1").all():
+    return 'It works.'
+  else:
+    return 'Something is broken.'
